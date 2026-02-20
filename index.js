763861f78c35455ad02954b69359c1e9ebe56fc4
coolify-kaspi <alemprofi2025@gmail.com>
2026-02-20 12:22:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send("Server is running 🚀");
 });
 
 app.listen(PORT, () => {
