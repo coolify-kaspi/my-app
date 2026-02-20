6b1b9c23ac88fc7f647a3a06b79a7368f4a5b6dd
coolify-kaspi <alemprofi2025@gmail.com>
2026-02-20 12:50:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-{
-  "name": "my-app",
-  "version": "1.0.0",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "express": "^4.19.2"
-  }
-}
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
